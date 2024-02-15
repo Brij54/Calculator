217b5b1c56a32c88754df5acfae9de91e156aa02
Brij54 <Brijesh.prajapati@iiitb.ac.in>
2024-02-15 09:55:20 +0530

    first change hii hello

--- src/main/java/com/example/devops/DevopsApplication.java
+++ src/main/java/com/example/devops/DevopsApplication.java
@@ -2,13 +2,50 @@ package com.example.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DevopsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsApplication.class, args);
+
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2, result;
+        char operator;
+
+        System.out.println("Enter first number:");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number:");
+        num2 = scanner.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /):");
+        operator = scanner.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error! Division by zero is not allowed.");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+        }
     }
-
-
 }
